1fa0b5003c0ae5466698958f3f245df2f03603e2
Khushitapariya <khushitap69@gmail.com>
2025-02-23 01:42:49 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,99 +1,140 @@
 import pickle
+import tensorflow
+import pandas as pd
 import json
 import random
+import tflearn
 import numpy as np
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-
 nltk.download('punkt')
 stemmer = LancasterStemmer()
 
-class Chatbot:
-    def __init__(self):
-        self.words = []
-        self.labels = []
-        self.training = []
-        self.output = []
-        self.model = None
+data = pd.read_json(
+    "")
 
-    def load_data(self, intents_file):
-        try:
-            with open(intents_file) as file:
-                data = json.load(file)
-        except FileNotFoundError:
-            print("Error: intents.json file not found.")
-            exit()
-        except json.JSONDecodeError:
-            print("Error: intents.json is not a valid JSON file.")
-            exit()
+try:
+    with open("FAQindents.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-        for intent in data["intents"]:
-            for pattern in intent["patterns"]:
-                tokenized_words = nltk.word_tokenize(pattern)
-                self.words.extend(tokenized_words)
-                self.training.append(tokenized_words)
-                self.output.append(intent['tag'])
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            # divides the complete string multiple 1 word strings
+            tokenized_words = nltk.word_tokenize(pattern)
+            words.extend(tokenized_words)
+            # all FAQ phrases correesponding to a tag are put into a list
+            docs_x.append(tokenized_words)
+            # docs_y corresponds to which FAQ belongs to which category....
+            docs_y.append(intent['tag'])
 
-            if intent['tag'] not in self.labels:
-                self.labels.append(intent['tag'])
+        if intent['tag'] not in labels:
+            labels.append(intent['tag'])
 
-        self.words = [stemmer.stem(w.lower()) for w in self.words if w != "?"]
-        self.words = sorted(list(set(self.words)))
-        self.labels = sorted(self.labels)
+    # Stem all the words to their root meaning
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
 
-        out_empty = [0 for _ in range(len(self.labels))]
-        for x, doc in enumerate(self.training):
-            bag = [1 if stemmer.stem(w.lower()) in doc else 0 for w in self.words]
-            output_row = out_empty[:]
-            output_row[self.labels.index(self.output[x])] = 1
-            self.training.append(bag)
-            self.output.append(output_row)
+    labels = sorted(labels)
 
-        self.training = np.array(self.training)
-        self.output = np.array(self.output)
+    training = []
+    output = []
 
-    def train_model(self, num_epochs=595, batch_size=8):
-        X_train, X_val, y_train, y_val = train_test_split(self.training, self.output, test_size=0.2, random_state=42)
-        self.model = Sequential()
-        self.model.add(Dense(20, input_shape=(len(self.training[0]), activation='relu'))
-        self.model.add(Dense(20, activation='relu'))
-        self.model.add(Dense(len(self.output[0]), activation='softmax'))
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-        self.model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=num_epochs, batch_size=batch_size)
+    """ Creating a list with 0s, each 0 represents a label.
+      The 0 will be incremented, if we identify some pattern
+      as a specific label. (One Hot Encoding)
+    """
+    out_empty = [0 for _ in range(len(labels))]
 
-    def chat(self):
-        print("Start talking with the bot <type quit to exit>")
-        while True:
-            inp = input("\n You:")
-            if inp.lower() == "quit":
-                break
-            results = self.model.predict([self.bag_of_words(inp)])
-            result_index = np.argmax(results)
-            pred_label = self.labels[result_index]
+    for x, doc in enumerate(docs_x):
+        bag = []
+        doc_x_stem = [stemmer.stem(w.lower()) for w in doc]
 
-            if results[0, result_index] > 0.65:
-                for tg in data["intents"]:
-                    if tg['tag'] == pred_label:
-                        response = tg['responses']
-                print("\nBot:", random.choice(response))
+        for w in words:
+            if w in doc_x_stem:
+                bag.append(1)
             else:
-                print("\nBot: I don't quite understand, try rephrasing your question")
+                bag.append(0)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-    def bag_of_words(self, s):
-        bag = [0 for _ in range(len(self.words))]
-        s_words = nltk.word_tokenize(s)
-        s_words = [stemmer.stem(word.lower()) for word in s_words]
-        for se in s_words:
-            for i, w in enumerate(self.words):
-                if w == se:
-                    bag[i] = 1
-        return np.array(bag)
+        training.append(bag)
+        output.append(output_row)
 
-if __name__ == "__main__":
-    chatbot = Chatbot()
-    chatbot.load_data("intents.json")
-    chatbot.train_model()
-    chatbot.chat()
+    training = np.array(training)
+    output = np.array(output)
+
+    with open("FAQindents.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+
+tensorflow.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 20)
+net = tflearn.fully_connected(net, 20)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    model.load("chatbot.tflearn")
+except:
+    model = tflearn.DNN(net)
+    model.fit(training, output, n_epoch=595, batch_size=8, show_metric=True)
+    model.save("chatbot.tflearn")
+
+
+def bag_of_words(s, words):
+    """
+    This function tokenizes and stems 
+    the words in the input sentence entered
+    by the user and converts it into a bag of
+    words.
+    """
+
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+    return np.array(bag)
+
+
+def chat():
+    """
+    This function provides a interface
+    between the model and the user for 
+    easy communication. It is also 
+    responsible for selecting the final
+    response from the choices provided 
+    by the json file 
+    """
+    print("Start talking with the bot <type quit to exit>")
+    while True:
+        inp = input("\n You:")
+        if(inp.lower() == "quit"):
+            break
+        results = model.predict([bag_of_words(inp, words)])
+        result_index = np.argmax(results)
+        pred_label = labels[result_index]
+
+        if(results[0, result_index] > 0.65):
+            for tg in data["intents"]:
+                if tg['tag'] == pred_label:
+                    response = tg['responses']
+            print("\n")
+            print("Bot: ", random.choice(response))
+        else:
+            print("\n")
+            print("Bot: I don't quite understand, try rephrasing your question")
+
+
+chat()
